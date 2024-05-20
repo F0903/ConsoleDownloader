e6ae44a39d41f878e52dea9e0024d8ef5c436e5b
Frederik P. <frepbh@gmail.com>
2024-05-20 18:57:53 +0200

    updated .net version to 8.0 and updated deps

--- ConsoleDownloaderClient/Program.cs
+++ ConsoleDownloaderClient/Program.cs
@@ -3,70 +3,63 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using ConsoleDownloader.Downloaders.YouTube;
+using ConsoleDownloaderClient;
 
-namespace ConsoleDownloaderClient
+static void PrintHeader()
 {
-    class Program
-    {
-        static readonly YouTubeDownloader youtube = new();
-
-        static void PrintHeader()
-        {
-            Console.WriteLine("Commands:");
-            Console.WriteLine("Download video: <url>\nDownload audio: -a <url>\nDownload thumbnail: -t <url>\n");
-        }
-
-        static async Task Run()
-        {
-            PrintHeader();
-            Console.Write("-> ");
-            string? input = Console.ReadLine();
-            if (input == null)
-                return;
-            try
-            {
-                await ArgumentHandler.HandleAsync(input.AsSpan());
-                Console.WriteLine("\nDone!");
-                await Task.Delay(1500);
-            }
-            catch (Exception ex)
-            {
-                const int msDelay = 7000;
-                Console.WriteLine($"Error: {ex.Message}\nClearing in {msDelay / 1000}s...");
-                await Task.Delay(msDelay);
-            }
-            Console.Clear();
-        }
-
-        static async Task SetupFFmpeg()
-        {
-            if (System.IO.File.Exists(FFmpeg.Path)) 
-                return;
-
-            Console.WriteLine("Downloading FFmpeg...");
-            await FFmpeg.DownloadAsync();
-            Console.Clear();
-
-            AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
-            {
-                FFmpeg.Delete();
-            };
-        }
-
-        static async Task Main()
-        {
-            ArgumentHandler.DefaultHandler = input => youtube.DownloadCombinedAsync(input, "./");
-            ArgumentHandler.Handlers = new Dictionary<char, ArgumentHandler.AsyncArgHandler>
-            {
-                {'a', input => youtube.DownloadAudioOnlyAsync(input, "./") },
-                {'t', input => youtube.DownloadThumbnailAsync(input, "./") }
-            };
-
-            Console.Title = "ConsoleDownloader";
-
-            await SetupFFmpeg();
-
-            while (true) await Run();
-        }
-    }
+    Console.WriteLine("Commands:");
+    Console.WriteLine("Download video: <url>\nDownload audio: -a <url>\nDownload thumbnail: -t <url>\n");
 }
+
+static async Task Run()
+{
+    PrintHeader();
+    Console.Write("-> ");
+    string? input = Console.ReadLine();
+    if (input == null)
+        return;
+    try
+    {
+        await ArgumentHandler.HandleAsync(input.AsSpan());
+        Console.WriteLine("\nDone!");
+        await Task.Delay(1500);
+    }
+    catch (Exception ex)
+    {
+        const int msDelay = 7000;
+        Console.WriteLine($"Error: {ex.Message}\nClearing in {msDelay / 1000}s...");
+        await Task.Delay(msDelay);
+    }
+    Console.Clear();
+}
+
+static async Task SetupFFmpeg()
+{
+    if (System.IO.File.Exists(FFmpeg.Path))
+        return;
+
+    Console.WriteLine("Downloading FFmpeg...");
+    await FFmpeg.DownloadAsync();
+    Console.Clear();
+
+    AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
+    {
+        FFmpeg.Delete();
+    };
+}
+
+
+var youtube = new YouTubeDownloader();
+
+ArgumentHandler.DefaultHandler = input => youtube.DownloadCombinedAsync(input, "./");
+ArgumentHandler.Handlers = new Dictionary<char, ArgumentHandler.AsyncArgHandler>
+{
+    {'a', input => youtube.DownloadAudioOnlyAsync(input, "./") },
+    {'t', input => youtube.DownloadThumbnailAsync(input, "./") }
+};
+
+Console.Title = "ConsoleDownloader";
+
+await SetupFFmpeg();
+
+while (true) await Run();
