e0f5df7be1bb371c51e3c46761e71804e4418dee
Frederik P. <frepbh@gmail.com>
2024-06-17 00:13:08 +0200

    completed auto update implementation

--- ConsoleDownloaderClient/Program.cs
+++ ConsoleDownloaderClient/Program.cs
@@ -1,126 +1,164 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using ConsoleDownloader.Downloaders.YouTube;
 using ConsoleDownloaderClient;
 
-static async Task SetupFFmpeg()
+namespace ConsoleDownloaderClient;
+public static class Program
 {
-    Console.WriteLine("Downloading FFmpeg...");
-    try
+    static void SetupCommands()
     {
-        await FFmpeg.EnsureAvailableAsync();
-        Console.Clear();
+        var youtube = new YouTubeDownloader();
+        AsyncCommandHandler.RegisterCommands(
+            new(
+                "download",
+                "Downloads media.\nOptions:\n-a Audio only\n-t Thumbnail only",
+                async (args) =>
+                {
+                    if (!FFmpeg.Downloaded)
+                        await SetupFFmpeg();
+
+                    string? url = null;
+                    bool audioOnly = false;
+                    bool thumbnailOnly = false;
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        var arg = args[i];
+                        switch (arg)
+                        {
+                            case "-a":
+                                audioOnly = true;
+                                continue;
+                            case "-t":
+                                thumbnailOnly = true;
+                                continue;
+                            default:
+                                url = arg;
+                                break;
+                        }
+                    }
+                    if (audioOnly && thumbnailOnly)
+                        throw new Exception("Cannot use both audio and thumbnail switch at the same time!");
+                    if (url is null)
+                        throw new NullReferenceException("No url specified!");
+
+                    if (audioOnly)
+                        await youtube.DownloadAudioOnlyAsync(url, "./");
+                    else if (thumbnailOnly)
+                        await youtube.DownloadThumbnailAsync(url, "./");
+                    else
+                        await youtube.DownloadCombinedAsync(url, "./");
+                }
+            ),
+            new(
+                "version",
+                "Gets the version number of the app.",
+                (args) =>
+                {
+                    Console.Write(Updater.CurrentVersion);
+                    return Task.CompletedTask;
+                }
+            ),
+            new(
+                "help",
+                "Prints commands.",
+                (args) =>
+                {
+                    Console.WriteLine("Commands:\n");
+                    var commands = AsyncCommandHandler.GetCommands();
+                    foreach (var cmdPair in commands)
+                    {
+                        var cmd = cmdPair.Value;
+                        Console.WriteLine($"{cmd.Name}\n{cmd.Description}");
+                        Console.WriteLine();
+                    }
+                    return Task.CompletedTask;
+                }
+            ),
+            new(
+                "clear",
+                "Clears the console.",
+                (args) =>
+                {
+                    Console.Clear();
+                    return Task.CompletedTask;
+                }
+            )
+        );
     }
-    catch (Exception ex)
+
+    static async Task SetupFFmpeg()
     {
-        Console.WriteLine($"Error downloading ffmpeg!\n{ex}");
-    }
-
-    AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
-    {
-        FFmpeg.Delete();
-    };
-}
-
-
-Console.Title = "ConsoleDownloader";
-
-var youtube = new YouTubeDownloader();
-
-AsyncCommandHandler.RegisterCommands(
-    new(
-        "download",
-        "Downloads media.\nOptions:\n-a Audio only\n-t Thumbnail only",
-        async (args) =>
+        Console.WriteLine("Downloading FFmpeg...");
+        try
         {
-            if (!FFmpeg.Downloaded)
-                await SetupFFmpeg();
+            await FFmpeg.EnsureAvailableAsync();
+            Console.Clear();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error downloading ffmpeg!\n{ex}");
+        }
 
-            string? url = null;
-            bool audioOnly = false;
-            bool thumbnailOnly = false;
+        AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
+        {
+            FFmpeg.Delete();
+        };
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.Title = "ConsoleDownloader";
+
+        string? oldAppPath = null;
+        if (args.Length > 0)
+        {
             for (int i = 0; i < args.Length; i++)
             {
                 var arg = args[i];
                 switch (arg)
                 {
-                    case "-a":
-                        audioOnly = true;
-                        continue;
-                    case "-t":
-                        thumbnailOnly = true;
-                        continue;
+                    case "--finish-update":
+                        oldAppPath = args[i + 1];
+                        break;
                     default:
-                        url = arg;
                         break;
                 }
             }
-            if (audioOnly && thumbnailOnly)
-                throw new Exception("Cannot use both audio and thumbnail switch at the same time!");
-            if (url is null)
-                throw new NullReferenceException("No url specified!");
+        }
 
-            if (audioOnly)
-                await youtube.DownloadAudioOnlyAsync(url, "./");
-            else if (thumbnailOnly)
-                await youtube.DownloadThumbnailAsync(url, "./");
+        try
+        {
+            if (oldAppPath is not null)
+                await Updater.FinishUpdate(oldAppPath);
             else
-                await youtube.DownloadCombinedAsync(url, "./");
+                await Updater.CheckUpdateAsync();
         }
-    ),
-    new (
-        "version", 
-        "Gets the version number of the app.",
-        (args) => 
-        { 
-            Console.Write(VersionControl.CurrentVersion);
-            return Task.CompletedTask;
-        }
-    ),
-    new (
-        "help", 
-        "Prints commands.", 
-        (args) =>
+        catch (Exception ex)
         {
-            Console.WriteLine("Commands:\n");
-            var commands = AsyncCommandHandler.GetCommands();
-            foreach (var cmdPair in commands)
-            {
-                var cmd = cmdPair.Value;
-                Console.WriteLine($"{cmd.Name}\n{cmd.Description}");
-                Console.WriteLine();
-            }
-            return Task.CompletedTask;
+            Console.WriteLine(ex.Message);
         }
-    ),
-    new (
-        "clear", 
-        "Clears the console.", 
-        (args) =>
-        {
-            Console.Clear();
-            return Task.CompletedTask;
-        }
-    )
-);
 
-await VersionControl.CheckUpdateAsync();
-await AsyncCommandHandler.ExecuteCommandAsync("help", []);
-while (true)
-{
-    Console.Write("-> ");
-    string? input = Console.ReadLine();
-    if (input == null)
-        continue;
-    try
-    {
-        var splitArgs = input.Split(' ');
-        await AsyncCommandHandler.ExecuteCommandAsync(splitArgs[0], splitArgs[1..]);
+        SetupCommands();
+        await AsyncCommandHandler.ExecuteCommandAsync("help", []);
+        while (true)
+        {
+            Console.Write("-> ");
+            string? input = Console.ReadLine();
+            if (input == null)
+                continue;
+            try
+            {
+                var splitArgs = input.Split(' ');
+                await AsyncCommandHandler.ExecuteCommandAsync(splitArgs[0], splitArgs[1..]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error!\n{ex.Message}\n");
+            }
+            Console.WriteLine();
+        }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error!\n{ex.Message}\n");
-    }
-    Console.WriteLine();
 }
---
+++ ConsoleDownloaderClient/Updater.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleDownloaderClient;
+public static class Updater
+{
+    static readonly Assembly currentAssembly = Assembly.GetExecutingAssembly();
+    static readonly string asmName = currentAssembly.GetName().Name!;
+    public static readonly Version CurrentVersion = currentAssembly.GetName().Version ?? throw new NullReferenceException("Version not defined!");
+
+    const string apiEndpoint = "https://api.github.com/repos/F0903/ConsoleDownloader/releases";
+
+    static async Task<(Version, string)> GetRemoteVersionAsync(HttpClient http)
+    {
+        Console.WriteLine("Checking for new version...");
+        var response = await http.GetAsync(apiEndpoint);
+        response.EnsureSuccessStatusCode();
+
+        using var stream = response.Content.ReadAsStream();
+        using var json = await JsonDocument.ParseAsync(stream);
+
+        var root = json.RootElement[0];
+        var tagName = root.GetProperty("tag_name").GetString()!;
+        var htmlUrl = root.GetProperty("html_url").GetString()!;
+        var assetsArray = root.GetProperty("assets").EnumerateArray();
+
+        var elem = assetsArray.First(x => x.TryGetProperty("name", out var name) && name.GetString() == $"{asmName}.exe");
+        var downloadUrl = elem.GetProperty("browser_download_url").GetString() ?? throw new NullReferenceException("Download url of new version was null!");
+        return (new(tagName), downloadUrl);
+    }
+
+    public static async Task CheckUpdateAsync()
+    {
+        try
+        {
+            var http = new HttpClient();
+            http.DefaultRequestHeaders.UserAgent.Add(new(asmName, CurrentVersion.ToString()));
+            var (remoteVersion, newVersionUrl) = await GetRemoteVersionAsync(http);
+            if (remoteVersion <= CurrentVersion)
+            {
+                return;
+            }
+
+            Console.WriteLine("A newer version is available!\nWould you like to download it? [y/N]");
+            var input = Console.ReadLine();
+            if (input is null || input.ToLower() is "n" or "no")
+            {
+                Console.WriteLine("New version ignored.");
+                return;
+            }
+            else if (input.ToLower() is "y" or "yes")
+            {
+                await DownloadNewVersion(http, remoteVersion, newVersionUrl);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Unable to get release info.");
+        }
+    }
+
+    static async Task DownloadNewVersion(HttpClient http, Version newVer, string url)
+    {
+        var fileName = $"{asmName}-{newVer}.exe";
+        {
+            Console.WriteLine("Downloading update...");
+            using var stream = await http.GetStreamAsync(url);
+            using var file = File.Create(fileName);
+            await stream.CopyToAsync(file);
+        }
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = fileName,
+            Arguments = $"--finish-update \"{asmName}.exe\"",
+            UseShellExecute = true,
+        });
+        Environment.Exit(0);
+    }
+
+    public static async Task FinishUpdate(string oldPath)
+    {
+        const int ATTEMPTS = 5;
+        for (int i = 0; i < ATTEMPTS; i++)
+        {
+            try
+            {
+                File.Delete(oldPath);
+                Console.WriteLine("Update successful!");
+                return;
+            }
+            catch (AccessViolationException)
+            {
+                await Task.Delay(500);
+                continue;
+            }
+        }
+        throw new Exception("Could not delete old version!");
+    }
+}
